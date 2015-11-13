418504d0a1be841b9037ccb9e2ffd803105b3c51
rastringer <ra1stringer@gmail.com>
2015-11-13 15:18:34 -0800

    adds sentiment test

--- test/sentiment_test.js
+++ test/sentiment_test.js
@@ -3,12 +3,20 @@ var expect = chai.expect;
 var sentiment = require(__dirname + "/../lib/index.js");
 
 describe('sentiment analysis', function(){
-  it('should return number scores of negative or positive', function(){
-    var negTestString = 'abandon disjointed frustrating mindless';
-    debugger;
-    var returnObject = sentiment(negTestString);
+  it('should return objects of words with number scores', function(){
+    var testString = 'abandon, disjointed';
+    var returnObject = sentiment(testString);
+    console.log(returnObject);
+    expect(returnObject).to.be.an('object');
+    expect(returnObject).to.eql(-2);
+    expect(returnObject.disjointed).to.eql('disjointed' -2);
+  });
+  it('should return average score of sentiment words divided by total score', function() {
+    var averageSentiment = 'abandon and disjointed';
+    var returnObject = sentiment(averageSentiment);
+    var returnAverage = sentiment(averageSentiment);
     console.log(typeof returnObject);
     expect(returnObject).to.be.an('object');
-    expect(returnObject).to.eql(-8);
+    expect(returnObject.averageSentiment).to.eql(-2);
   });
 });
