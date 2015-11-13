e546962432efd84e3428fcb1bb3b823fa736c268
rastringer <ra1stringer@gmail.com>
2015-11-13 10:49:05 -0800

    adds test, stopwords

---
+++ test/sentiment_test.js
@@ -0,0 +1,13 @@
+var chai = require('chai')
+var expect = chai.expect;
+var sentiment = require(__dirname + "/../lib/index.js");
+
+describe('sentiment analysis', function(){
+  it('should return number scores of negative or positive', function(){
+    var negTestString = 'abandon disjointed frustrating mindless'
+    var returnObject = sentiment(negTestString);
+    console.log(typeof returnObject);
+    expect(returnObject).to.be.an('object');
+    expect(returnObject).to.eql(-8);
+  });
+});
