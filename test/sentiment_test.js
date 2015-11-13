540b9b91ee0dec6276af98e091ae16385ff87fec
timcmiller <timcmiller@live.com>
2015-11-13 13:56:12 -0800

    working sentiment funciton

--- test/sentiment_test.js
+++ test/sentiment_test.js
@@ -1,10 +1,11 @@
-var chai = require('chai')
+var chai = require('chai');
 var expect = chai.expect;
 var sentiment = require(__dirname + "/../lib/index.js");
 
 describe('sentiment analysis', function(){
   it('should return number scores of negative or positive', function(){
-    var negTestString = 'abandon disjointed frustrating mindless'
+    var negTestString = 'abandon disjointed frustrating mindless';
+    debugger;
     var returnObject = sentiment(negTestString);
     console.log(typeof returnObject);
     expect(returnObject).to.be.an('object');
