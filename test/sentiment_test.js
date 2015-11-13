91abbc631a67bd1f1bade2900ae7759fdcccc4f9
rastringer <ra1stringer@gmail.com>
2015-11-13 15:34:01 -0800

    tests working

--- test/sentiment_test.js
+++ test/sentiment_test.js
@@ -3,20 +3,15 @@ var expect = chai.expect;
 var sentiment = require(__dirname + "/../lib/index.js");
 
 describe('sentiment analysis', function(){
-  it('should return objects of words with number scores', function(){
+  it('should take string input and return sentiment levels', function() {
     var testString = 'abandon, disjointed';
     var returnObject = sentiment(testString);
-    console.log(returnObject);
     expect(returnObject).to.be.an('object');
-    expect(returnObject).to.eql(-2);
-    expect(returnObject.disjointed).to.eql('disjointed' -2);
-  });
-  it('should return average score of sentiment words divided by total score', function() {
-    var averageSentiment = 'abandon and disjointed';
-    var returnObject = sentiment(averageSentiment);
-    var returnAverage = sentiment(averageSentiment);
-    console.log(typeof returnObject);
-    expect(returnObject).to.be.an('object');
-    expect(returnObject.averageSentiment).to.eql(-2);
+    expect(returnObject.sentimentValue).to.eql(-2);
+    expect(returnObject).to.have.property('negTerms');
+    expect(returnObject).to.have.property('vNegTerms');
+    expect(returnObject).to.have.property('posTerms');
+    expect(returnObject).to.have.property('vPosTerms');
+    expect(returnObject.sentiment).to.eql('Mildly Negative');
   });
 });
