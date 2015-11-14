752ce2e187b44d9b863691f81af5365f0fe97e97
Kenji Crosland <kenji.crosland@gmail.com>
2015-11-13 16:07:24 -0800

    Can save to db

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var frequency = require(__dirname + '/lib/frequency');
 var collectionRouter = require(__dirname + '/routes/collection_routes.js');
 var articleRouter = require(__dirname + '/routes/article_routes.js');
+var Article = require(__dirname + '/models/article').Article;
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/nlp_database');
 
@@ -17,8 +18,21 @@ app.get('/', function(req, res){
 });
 
 app.post('/process', bodyParser.urlencoded({extended: true}), function(req, res){
-  var recievedText = (req.body.text);
-  var returnJSON = JSON.stringify(frequency(recievedText));
+  var frequencyObject = frequency((req.body.text));
+  var mappedArray = [];
+  var i = 0;
+  for (var key in frequencyObject){
+    mappedArray[i] = {};
+    mappedArray[i].word = key;
+    mappedArray[i].count = frequencyObject[key];
+    i++;
+  }
+  var newArticle = new Article({title: "Default Title", wordcounts: mappedArray});
+  var returnJSON = JSON.stringify(frequencyObject);
+  newArticle.save(function(err, data) {
+    if(err) throw err;
+    console.log('Saved!')
+  });
   res.send(returnJSON);
 });
 
