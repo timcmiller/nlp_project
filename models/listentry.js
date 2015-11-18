8855b81a09537b454670d4076c141ee992beda11
Kenji Crosland <kenji.crosland@gmail.com>
2015-11-18 14:30:45 -0800

    Fix population bug

--- models/listentry.js
+++ models/listentry.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var listSchema = require(__dirname + '/list.js').listSchema;
 var articleSchema = require(__dirname + '/article.js').articleSchema;
-
+var Article = require(__dirname + '/article.js').Article;
+var List = require(__dirname + '/list.js').List;
 //When creating a new list entry with a post request it updates both the "articles" array in the List model as well as the "lists" array in the Articles model. Deleting removes from both as well.
 
 var listEntrySchema = mongoose.Schema({
@@ -11,3 +12,22 @@ var listEntrySchema = mongoose.Schema({
 });
 
 var ListEntry = module.exports.ListEntry = mongoose.model('ListEntry', listEntrySchema);
+var updateListandArticle = module.exports.updateListAndArticle = function(entry){
+  //if (addOrRemove === 'add' || addOrRemove === undefined){
+    Article.findByIdAndUpdate(entry.article, {$push: {"lists": entry.list}}, function(err, model){
+      if (err) throw err;
+    });
+    List.findByIdAndUpdate(entry.list, {$push: {"articles": entry.article}}, function(err, model){
+      if (err) throw err;
+    });
+ // }
+  //TODO: Implement this.
+  //  else if (addOrRemove === 'remove'){
+  //   Article.update({_id: doc.article}, {$pull: {lists: entry.list}}, function(err, entry){
+  //     if (err) throw err;
+  //   });
+  //   List.update({_id: doc.list}, {$pull: {articles: entry.article}}, function(err, entry){
+  //     if (err) throw err;
+  //   });
+  // }
+}
