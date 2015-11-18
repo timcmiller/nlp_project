82d835cccbdfd755e825b2481b92d867756f95eb
Kenji Crosland <kenji.crosland@gmail.com>
2015-11-18 09:39:47 -0800

    New lists test passed

--- test/routes_test.js
+++ test/routes_test.js
@@ -128,6 +128,15 @@ describe('the list routes', function() {
       });
     });
 
+    it('should be able to get a single list', function(){
+      chai.request('localhost:3000')
+      .get('/api/lists/' + this.list._id)
+      .end(function(err, res){
+        expect(err).to.eql(null);
+        expect(res.body.name).to.eql('test');
+      });
+    });
+
     it('should be able to delete a list', function() {
       chai.request('localhost:3000')
       .delete('/api/lists/' + this.list._id)
